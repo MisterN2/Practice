7d3a4a11cf26deb4ed2d9c6ab66bc4d5fdb602dd
MisterN <nesynandriy6@gmail.com>
2025-09-15 16:35:56 +0300

    Add files via upload

---
+++ Book class/src/Main.java
@@ -0,0 +1,53 @@
+
+ class Book {
+    String title;
+    String author;
+    boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true; // нова книжка доступна
+    }
+}
+
+
+class Library {
+    Book book;
+
+    public void addBook(Book b) {
+        book = b;
+        System.out.println("Book added: " + b.title);
+    }
+
+    public void checkOutBook() {
+        if (book != null && book.isAvailable) {
+            book.isAvailable = false;
+            System.out.println("You checked out: " + book.title);
+        } else {
+            System.out.println("Book not available.");
+        }
+    }
+
+    public void returnBook() {
+        if (book != null && !book.isAvailable) {
+            book.isAvailable = true;
+            System.out.println("You returned: " + book.title);
+        } else {
+            System.out.println("No book to return.");
+        }
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        Library lib = new Library();
+        Book b1 = new Book("Java Basics", "John Smith");
+
+        lib.addBook(b1);
+        lib.checkOutBook();
+        lib.checkOutBook();
+        lib.returnBook();
+    }
+}
